245eb95deff7984a99b85e233dbb9dc1974010fc
qiyubrother <13240137763@163.com>
2020-10-15 18:18:21 +0800

    添加项目文件。

---
+++ RtmpVLCDemo/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace RtmpVLCDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.videoView1 = new LibVLCSharp.WinForms.VideoView();
+            ((System.ComponentModel.ISupportInitialize)(this.videoView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // videoView1
+            // 
+            this.videoView1.BackColor = System.Drawing.Color.Black;
+            this.videoView1.Location = new System.Drawing.Point(12, 12);
+            this.videoView1.MediaPlayer = null;
+            this.videoView1.Name = "videoView1";
+            this.videoView1.Size = new System.Drawing.Size(532, 296);
+            this.videoView1.TabIndex = 0;
+            this.videoView1.Text = "videoView1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.videoView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.videoView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private LibVLCSharp.WinForms.VideoView videoView1;
+    }
+}
+
---
+++ RtmpVLCDemo/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RtmpVLCDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // install-package LibVLCSharp.WinForms
+        // install-package VideoLAN.LibVLC.Windows
+
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+
+            LibVLCSharp.Shared.Core.Initialize();
+            var libvlc = new LibVLCSharp.Shared.LibVLC();
+            videoView1.MediaPlayer = new LibVLCSharp.Shared.MediaPlayer(libvlc);
+            var media = new LibVLCSharp.Shared.Media(libvlc, new Uri("rtmp://58.200.131.2:1935/livetv/hunantv"));
+            videoView1.MediaPlayer.Play(media);
+        }
+    }
+}
